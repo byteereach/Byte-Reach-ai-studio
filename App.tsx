1d9df559cfc722d09db9bee3f38a0fd1b8c034e1
byteereach <byteereach@gmail.com>
2025-11-20 23:08:03 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,32 +1,27 @@
-import React from 'react';
-import { Header } from './components/Header';
-import { Hero } from './components/Hero';
-import { About } from './components/About';
-import { Services } from './components/Services';
-import { Portfolio } from './components/Portfolio';
-import { Results } from './components/Results';
-import { Testimonials } from './components/Testimonials';
-import { Contact } from './components/Contact';
-import { Footer } from './components/Footer';
-import { FloatingWhatsApp } from './components/FloatingWhatsApp';
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Portfolio from "./components/Portfolio";
+import Results from "./components/Results";
+import Testimonials from "./components/Testimonials";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import FloatingWhatsApp from "./components/FloatingWhatsApp";
 
-const App: React.FC = () => {
+export default function App() {
   return (
-    <div className="min-h-screen flex flex-col relative overflow-x-hidden">
+    <div>
       <Header />
-      <main className="flex-grow">
-        <Hero />
-        <About />
-        <Services />
-        <Portfolio />
-        <Results />
-        <Testimonials />
-        <Contact />
-      </main>
+      <Hero />
+      <Services />
+      <Portfolio />
+      <Results />
+      <Testimonials />
+      <About />
+      <Contact />
       <Footer />
       <FloatingWhatsApp />
     </div>
   );
-};
-
-export default App;
+}
